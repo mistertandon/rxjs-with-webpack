9788499d6bd6903af62d0285487b1203e6d35583
Mister Tandon <enggparveshtandon@gmail.com>
2017-01-05 22:12:45 +0530

    Set boiler plate to start with RxJx basics.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
 var config = {
     devServer: {
         host: '127.0.0.1',
-        port: 3468
+        port: 3580
     },
     module: {
-        preLoaders: [
-            {test: /\.js$/, exclude: /(node_modules|bower_component)/, loader: "jshint-loader"}
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_component)/,
-                loader: "babel",
-                query: {
-                    presets: ['es2015']
-                }
-            },
-            {test: /\.css$/, exclude: /(node_modules|bower_component)/, loaders: ["style-loader", "css-loader"]}
-        ]
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_component)/,
+            loader: "babel",
+            query: {
+                presets: ['es2015']
+            }
+        }, {
+            test: /\.css$/,
+            exclude: /(node_modules|bower_component)/,
+            loaders: ["style-loader", "css-loader"]
+        }]
     },
     devtool: "source-map"
 };
 
-var rxjs = Object.assign(
-        {},
-        config,
-        {
-            entry: {
-                dist_rxjs_1: ["./src/js/rxjs/src_promise"]
-            },
-            output: {
-                path: './webpack_transpiled/bluebird_promise',
-                filename: "[name].js"
-            }
-        });
+var rxjs = Object.assign({},
+    config, {
+        entry: {
+            dist_rxjs_ui_events: ["./src/js/rxjs/dist_rxjs_ui_events"]
+        },
+        output: {
+            path: './webpack_transpiled/rxjs',
+            filename: "[name].js"
+        }
+    });
 
 
 /**
